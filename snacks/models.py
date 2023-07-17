8403c339d8b6cb637de0092c8e78be653c215c19
eman obeidat <amoonobeidat43@gmail.com>
2023-07-17 17:24:41 +0000

    create done

--- snacks/models.py
+++ snacks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 # Create your models here.
 class Snack(models.Model):
     title = models.CharField(max_length=255)
@@ -9,3 +10,5 @@ class Snack(models.Model):
 
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('snack_detail',args=[self.id])
--- snacks/views.py
+++ snacks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
 from .models import Snack
+from django.urls import reverse_lazy, reverse
 # Create your views here.
 class SnackListView(ListView):
     template_name='snacks_list.html'
@@ -8,3 +9,13 @@ class SnackListView(ListView):
 class SnackDetailView(DetailView):
     template_name='snacks_detail.html'
     model=Snack
+class SnackCreateView(CreateView):
+    template_name='snacks_create.html'
+    model=Snack
+    fields=['title', 'purchaser', 'description','reviewer']
+
+class SnackUpdateView(UpdateView):
+        template_name = 'snacks_update.html'
+        model = Snack
+        fields='__all__'
+        success_url = reverse_lazy('snack_list')
